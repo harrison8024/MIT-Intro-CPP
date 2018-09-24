e58d39b1f4d6dc9ccfe765e10c22c0d310647926
harrison8024 <harrison8024@gmail.com>
2018-09-23 20:40:18 -0700

    added 3.4.3

--- 3-4.cpp
+++ 3-4.cpp
@@ -16,6 +16,25 @@ using namespace std;
 // }
 
 //3.4.2
+// int main()
+// {
+//     while(1)
+//     {
+//         int N;
+//         cout << "Enter an Integer:" << endl;
+//         cin >> N;
+//         if(N%5 > 0)
+//         {
+//             cout << "-1\n";
+//             continue;
+//         } 
+//         cout << N/5 << "\n";
+//     }
+//     return 0;
+// }
+
+
+//3.4.3
 int main()
 {
     while(1)
@@ -29,6 +48,10 @@ int main()
             continue;
         } 
         cout << N/5 << "\n";
+        if(N == -1){
+            break;
+        }
     }
+    cout << "Good Bye!" << endl;
     return 0;
 }
